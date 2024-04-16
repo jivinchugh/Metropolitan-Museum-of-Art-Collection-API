9649a1e5e61575b14be538bda1969457c1ba8e36
Your Name <jchugh@myseneca.ca>
2024-04-16 03:19:53 -0400

    intial commit

--- pages/search.js
+++ pages/search.js
@@ -4,6 +4,7 @@ import { useForm } from 'react-hook-form';
 import { useAtom } from 'jotai'; // Import useAtom hook
 import { searchHistoryAtom } from '../store'; // Import searchHistoryAtom
 import { addToHistory } from '@/lib/userData';
+import Image from 'next/image';
 
 const AdvancedSearch = () => {
     const router = useRouter();
@@ -49,7 +50,7 @@ const AdvancedSearch = () => {
                         <Form.Label>Geo Location</Form.Label>
                         <Form.Control type="text" placeholder="" name="geoLocation" {...register("geoLocation")} />
                         <Form.Text className="text-muted">
-                            Case Sensitive String (ie "Europe", "France", "Paris", "China", "New York", etc.), with multiple values separated by the | operator
+                            Case Sensitive String (ie &quot;Europe&quot;, &quot;France&quot;, &quot;Paris&quot;, &quot;China&quot;, &quot;New York&quot;, etc.), with multiple values separated by the | operator
                         </Form.Text>
                     </Form.Group>
                 </Col>
@@ -58,7 +59,7 @@ const AdvancedSearch = () => {
                         <Form.Label>Medium</Form.Label>
                         <Form.Control type="text" placeholder="" name="medium" {...register("medium")} />
                         <Form.Text className="text-muted">
-                            Case Sensitive String (ie: "Ceramics", "Furniture", "Paintings", "Sculpture", "Textiles", etc.), with multiple values separated by the | operator
+                            Case Sensitive String (ie: &quot;Ceramics&quot;, &quot;Furniture&quot;, &quot;Paintings&quot;, &quot;Sculpture&quot;, &quot;Textiles&quot;, etc.), with multiple values separated by the | operator
                         </Form.Text>
                     </Form.Group>
                 </Col>
